f306d9bfaca7c039ff9a9ca04728585ad0fae251
mann-prajapati05 <mannprajapati0284@gmail.com>
2026-04-16 22:43:37 +0530

    update cors

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app=express();
 
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: "https://talksy-ai-frontend.onrender.com", 
     credentials: true                
 }));
 app.use(cookieParser());
